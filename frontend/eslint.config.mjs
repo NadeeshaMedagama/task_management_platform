a830bd02b3d3577198fb40ee77980c4d82027f46
NadeeshaMedagama <nadeeshamedagama@gmail.com>
2026-03-21 10:05:41 +0530

    kept native flag config style with eslint and switched to name config export

--- frontend/eslint.config.mjs
+++ frontend/eslint.config.mjs
@@ -1,18 +1,10 @@
-import { FlatCompat } from "@eslint/eslintrc";
-import { dirname } from "node:path";
-import { fileURLToPath } from "node:url";
+import { defineConfig, globalIgnores } from "eslint/config";
+import nextVitals from "eslint-config-next/core-web-vitals";
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
-
-const compat = new FlatCompat({
-  baseDirectory: __dirname,
-});
-
-export default [
-  {
-    ignores: [".next/**", "out/**", "build/**", "next-env.d.ts"],
-  },
-  ...compat.extends("next/core-web-vitals"),
+const config = [
+  ...nextVitals,
+  globalIgnores([".next/**", "out/**", "build/**", "next-env.d.ts"]),
 ];
+
+export default config;
 
